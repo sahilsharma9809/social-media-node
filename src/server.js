8a935df3953aaf155ed7dc626c970a4971e2bfed
sahilsharma9809 <shanu15sahil@gmail.com>
2021-01-10 19:03:34 +0530

    Port

--- src/server.js
+++ src/server.js
@@ -12,11 +12,13 @@ app.use('/api/users', usersRoute)
 app.use('/api/posts', postsRoute)
 app.use('/', express.static(__dirname + '/public'))
 
+const PORT =process.env.PORT || 3002
+ 
 db.sync()
   .then(() => {
-    app.listen(8384, () => {
-      console.log('server started on http://localhost:8384')
-    })
+    app.listen(PORT,()=>{
+      console.log(`server started at http://localhost:${PORT}`);
+  })
   })
   .catch((err) => {
     console.error(new Error('Could not start database'))
